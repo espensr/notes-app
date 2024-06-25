1e5155371d5ed856d7ad0c1f28d9f8b791dd062f
Espen Strand Rørvik <espenror@hotmail.com>
2024-06-25 14:33:20 +0200

    Add metadata

--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -49,6 +49,13 @@ export function links() {
   return [...newNoteLinks(), ...noteListLinks()]
 }
 
+export function meta() {
+  return {
+    title: 'All Notes',
+    description: 'Manage your notes with ease.'
+  }
+}
+
 export function CatchBoundary() {
   const caughtResponse = useCatch()
 
